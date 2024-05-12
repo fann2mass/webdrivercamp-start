c59b1fc9167476114e66b45dac031dfa5d49f9d2
fann2mass <ifann2mass@gmail.com>
2024-05-12 16:58:13 -0700

    Task 1.Add Some Numbers is Done

--- 1_adding_some_numbers.py
+++ 1_adding_some_numbers.py
@@ -1,4 +1,4 @@
 number = 100
 print("Learning Python is fun" + '"' + "'" " - " "100 %")
 print("Learning Python is fun" + '"' + "'" " -",number, "%")
-print("Learning Python is fun" + '"' + "'" " -",format(number), "%")
+print("Learning Python is fun" + '"' + "'" " -",format(number), "%") 
